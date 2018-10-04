1977b4dab1b33927efb5a4ec205e09e49cbd5214
aspowerr <31863149+aspowerr@users.noreply.github.com>
2018-10-05 00:52:23 +0300

    all passing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  
+  let Base = {}; 
+  let Number = {}; 
+  let p = 0;
+  
+  for(let i = 2; i <= base; i++)
+  {
+    while(base % i === 0){
+      if(Base[i]) {
+        Base[i]++;
+      } 
+      else {
+        Base[i] = 1;
+      }
+
+    base = base / i;
+    }
+  }
+
+for(let j in Base)
+{
+  Number[j] = 0;
+  for(let i = 1; i < number; i++)
+    {
+      let pow = Math.pow(+j, i);
+      p = Math.floor( number / pow );
+
+    if(p !== 0) 
+    {
+      Number[j] += p;
+    }
+   else break;
+    }
 }
+
+let result = [];
+
+for(let j in Base)
+{
+  let floor = Math.floor(Number[j]/Base[j]);
+  result.push(floor);
+}
+
+return Math.min.apply(null,result);
+} 
